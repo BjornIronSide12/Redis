fc4f0440985e909b09f764adbc5c6cc74a0dc922
Raghvendra <raghvendrasonarikar@gmail.com>
2024-11-16 16:05:38 +0000

    Respond to multiple PINGs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,8 +24,9 @@ public class Main {
          serverSocket.setReuseAddress(true);
          // Wait for connection from client.
          clientSocket = serverSocket.accept();
+         processMultipleRequests(clientSocket);
          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-       } catch (IOException e) {
+        } catch (IOException e) {
          System.out.println("IOException: " + e.getMessage());
        } finally {
          try {
@@ -30,5 +37,18 @@ public class Main {
            System.out.println("IOException: " + e.getMessage());
          }
        }
+  }
+  private static void processMultipleRequests(Socket clienSocket) throws IOException {
+      try(BufferedReader reader = new BufferedReader(new InputStreamReader(clienSocket.getInputStream()));
+          BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clienSocket.getOutputStream()));) {
+
+            String redisCommand;
+            while((redisCommand = reader.readLine()) != null) {
+              if("ping".equalsIgnoreCase(redisCommand)) {
+                writer.write("+PONG\r\n");
+                writer.flush();
+              }
+            }
+          }
   }
 }
